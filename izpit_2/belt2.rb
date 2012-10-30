820950583709aae2bdc715aa797e4ea0da1ef4b8
Stefan <spetrov123@yahoo.com>
2012-10-30 10:23:46 +0200

    yellow belt task

---
+++ izpit_2/belt2.rb
@@ -0,0 +1,16 @@
+require 'csv'
+
+sum = 0.0
+output_file = "output"+"feature_scenarios_times.csv"
+
+CSV.open("new_file", "w") do |csv|
+	CSV.foreach("/home/fail/izpit_2/feature_scenarios_times.csv") do |row|
+		if (row[0].to_f > 5)
+			sum = sum + row[0].to_f	
+		end
+		csv << [sum]
+	end
+end
+p sum
+
+
